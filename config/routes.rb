07f8895acc363bd6e0ab1a8d3d3a7e6bd43ba41b
Chen Jo Wei <chenjowei@Chenteki-MacBook-Air-2.local>
2017-03-07 14:52:50 +0800

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+  resources :photos
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
 end
